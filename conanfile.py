0e68a780dfcdbdb05c38cd8d73fe3a3dcec739d1
Thomas Nibler <dev@tnibler.de>
2021-09-03 22:20:02 +0200

    pkg_config stuff

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class GstpluginsbaseConan(ConanFile):
     name = "gst_plugins_base"
-    version = "1.18.4"
+    version = "1.19.1"
     description = ""
     url = "https://github.com/GStreamer/gst-plugins-base"
     homepage = "https://github.com/GStreamer/gst-plugins-base"
@@ -14,14 +14,11 @@ class GstpluginsbaseConan(ConanFile):
     default_options = {
         "shared": True
     }
-    generators = "cmake"
+    generators = "cmake", "pkg_config"
     requires = (
-        "gstreamer/1.18.4",
+        "gstreamer/1.19.1",
         "gobject-introspection/1.68.0",
         "libffi/3.4.2",
-        # "gst-plugins-base/1.18.4",
-        # "gst-plugins-good/1.18.4",
-        # "gst-plugins-bad/1.18.4",
     )
     source_subfolder = "gst-plugins-base"
     build_subfolder = "build"
@@ -48,14 +45,34 @@ class GstpluginsbaseConan(ConanFile):
         #                 source_folder=self.source_subfolder)
         # meson.build()
         self.run("ninja install --verbose")
+        for pc_file in os.listdir("install/lib/pkgconfig"):
+            tools.replace_prefix_in_pc_file(
+                f"install/lib/pkgconfig/{pc_file}", "${package_root_path_gst_plugins_base}")
 
     def package(self):
-        print(os.getcwd())
         self.copy("lib/*", "", "install")
         self.copy("include/*", "", "install")
         self.copy("bin/*", "", "install")
         self.copy("share/*", "", "install")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ['include/gstreamer-1.0']
+        self.cpp_info.components['gst-plugins-base'].libs = tools.collect_libs(
+            self)
+        self.cpp_info.components['gst-plugins-base'].includedirs = [
+            'include/gstreamer-1.0']
+        self.cpp_info.components['gst-plugins-base'].requires = [
+            'gstreamer::gstreamer-1.0', 'gobject-introspection::gobject-introspection', 'libffi::libffi']
+
+        self.cpp_info.components['gstreamer-pbutils-1.0'].names['pkg_config'] = 'gstreamer-pbutils-1.0'
+        self.cpp_info.components['gstreamer-pbutils-1.0'].names['cmake_find_package'] = 'gstreamer-pbutils-1.0'
+        self.cpp_info.components['gstreamer-pbutils-1.0'].requires = [
+            'gst-plugins-base', 'gstreamer::gstreamer-1.0']
+        self.cpp_info.components['gstreamer-pbutils-1.0'].libs = ['gstpbutils-1.0']
+
+        self.cpp_info.components['gstreamer-audio-1.0'].names['pkg_config'] = 'gstreamer-audio-1.0'
+        self.cpp_info.components['gstreamer-audio-1.0'].names['cmake_find_package'] = 'gstreamer-audio-1.0'
+        self.cpp_info.components['gstreamer-audio-1.0'].requires = [
+            'gst-plugins-base', 'gstreamer::gstreamer-1.0']
+        self.cpp_info.components['gstreamer-audio-1.0'].libs = ['gstaudio-1.0']
+        self.cpp_info.components['gstreamer-audio-1.0'].includedirs = [
+            'include/gstreamer-1.0']
